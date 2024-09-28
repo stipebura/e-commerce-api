8a62d8f9953d7ad8c3dff51b7a63cd265fa71662
stipebura <dev.stipebura@gmail.com>
2024-09-28 17:01:51 +0200

    added .env file, mongoDB url

--- back-end/app.js
+++ back-end/app.js
@@ -1,1 +1,15 @@
-console.log('E-Commerce API');
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 5000;
+const DB = require("./db/connect");
+const start = async () => {
+  try {
+    await DB(process.env.MONGO_URL);
+    app.listen(
+      port,
+      console.log(`Server is connected to db and listening on port ${port}`)
+    );
+  } catch (error) {}
+};
+start();
