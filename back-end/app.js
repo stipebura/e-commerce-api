b1d79a4aa02a8c0a14039fe73be1e3addfcdc3dd
stipebura <dev.stipebura@gmail.com>
2024-09-28 22:55:29 +0200

    added package for try/catch, notFound and ErrorHandler middleware

--- back-end/app.js
+++ back-end/app.js
@@ -1,8 +1,17 @@
 require("dotenv").config();
+require("express-async-errors");
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 const DB = require("./db/connect");
+const errorHandlerMiddleware = require("./middleware/error-handler");
+const notFoundMiddleware = require("./middleware/not-found");
+app.use(express.json());
+app.get("/", (req, res) => {
+  res.send("e-commerce");
+});
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
 const start = async () => {
   try {
     await DB(process.env.MONGO_URL);
