08da7d1fe558eef3bb4e0835463d06222b99ddde
stipebura <dev.stipebura@gmail.com>
2024-09-30 14:23:33 +0200

    added jwt, hashing and comparing password

--- back-end/controllers/authController.js
+++ back-end/controllers/authController.js
@@ -1,14 +1,22 @@
 const User = require("../models/User");
 const { StatusCodes } = require("http-status-codes");
+const jwt = require("jsonwebtoken");
 const CustomError = require("../errors");
 const register = async (req, res) => {
-  const { email } = req.body;
+  const { email, name, password } = req.body;
   const emailAlreadyExists = await User.findOne({ email });
   if (emailAlreadyExists) {
     throw new CustomError.BadRequestError("Email is already used");
   }
-  const user = await User.create(req.body);
-  res.status(StatusCodes.CREATED).json({ user });
+  const isFirstUser = (await User.countDocuments({})) === 0;
+  const role = isFirstUser ? "admin" : "user";
+
+  const user = await User.create({ email, name, password, role });
+  const tokenUser = { name: user.name, userID: user._id, role: user.role };
+  const token = jwt.sign(tokenUser, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_LIFETIME,
+  });
+  res.status(StatusCodes.CREATED).json({ user: tokenUser, token });
 };
 const login = async (req, res) => {
   res.send("login");
