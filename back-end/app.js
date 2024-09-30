fd130d0c2d97d9b755020322f7010f7d5f201db8
stipebura <dev.stipebura@gmail.com>
2024-09-30 19:15:41 +0200

    signed cookie

--- back-end/app.js
+++ back-end/app.js
@@ -11,7 +11,7 @@ const cookieParser = require("cookie-parser");
 const morgan = require("morgan");
 app.use(morgan("tiny"));
 app.use(express.json());
-app.use(cookieParser());
+app.use(cookieParser(process.env.JWT_SECRET));
 app.use("/api/v1/auth", authRouter);
 app.get("/", (req, res) => {
   res.send("e-commerce");
