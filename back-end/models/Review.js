e9f1f6481d8bd3c552d363b72cccec5acbcb2ae2
stipebura <dev.stipebura@gmail.com>
2024-10-19 12:44:34 +0200

    aggreagetion

--- back-end/models/Review.js
+++ back-end/models/Review.js
@@ -33,4 +33,39 @@ const ReviewSchema = new mongoose.Schema(
   { timestamps: true }
 );
 ReviewSchema.index({ product: 1, user: 1 }, { unique: true });
+ReviewSchema.statics.calculateAverageRating = async function (productID) {
+  const result = await this.aggregate([
+    { $match: { product: productID } },
+    {
+      $group: {
+        _id: null,
+        averageRating: { $avg: "$rating" },
+        numOfReviews: { $sum: 1 },
+      },
+    },
+  ]);
+  console.log(result);
+
+  try {
+    await this.model("Product").findOneAndUpdate(
+      { _id: productID },
+      {
+        averageRating: Math.ceil(result[0]?.averageRating || 0),
+        numOfReviews: result[0]?.numOfReviews || 0,
+      }
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+ReviewSchema.post("save", async function () {
+  await this.constructor.calculateAverageRating(this.product);
+});
+ReviewSchema.post(
+  "deleteOne",
+  { document: true, query: false },
+  async function () {
+    await this.constructor.calculateAverageRating(this.product);
+  }
+);
 module.exports = mongoose.model("Review", ReviewSchema);
